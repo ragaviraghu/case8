7beb78070ce2f676c14b8ab840c5185e4881f170
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-15 11:02:46 -0700

    Create multiple_thir.cpp

---
+++ multiple_thir.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int N,i;
+cout<<"Enter a number : "<<endl;
+cin>>N;
+if(N%13==0)
+cout<<"Yes(The given number is a multiple of 13)"<<endl;
+else
+cout<<"No"<<endl;
+return 0;
+}
