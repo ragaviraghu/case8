41007ececb570544dd2ad2bf4c62910f45cf7e70
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-19 06:14:36 -0700

    Create round.cpp

---
+++ round.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+double d;
+cout<<"Enter the decimal : "<<endl;
+cin>>d;
+cout<<"Round off value : "<<ceil(d)<<endl;
+return 0;
+}
